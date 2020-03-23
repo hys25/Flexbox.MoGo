fd02dcb2de8b0a15d4ac337516422afd90531e98
Halyna <galya.yavtushenko@gmail.com>
2020-03-23 18:11:01 +0200

    Adaptive layout of MoGo's website.HTML/CSS/JS

---
+++ assets/JS/app.js
@@ -0,0 +1,80 @@
+$(function(){
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+/* Fixed header */
+     checkScroll(scrollOffset);
+
+    $(window).on("scroll", function(){
+
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if(scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+/* Smooth scroll */
+    $("[data-scroll]").on("click", function(event){
+     event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+
+
+        $("html,body").animate({
+            scrollTop: blockOffset
+        },900);
+    });
+
+    /* Menu nav toggle */
+
+    $("#nav_toggle").ont){
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+
+        $("#nav").toggleClass("active");
+    })
+
+/* Collapse */
+$("[data-collapse]").on("click",function(event){
+    event.preventDefault();
+
+     var $this = $(this),
+         blockId = $this.data('collapse');
+
+
+    $(blockId).slideToggle();
+
+         });
+
+/* Slider */
+$("[data-slider]").slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+
+
+});
+
+
+
+$(function(){
+    function myFunction {document.write("Hello world!");
+});
